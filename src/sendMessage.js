4f9f16ef1a46a2a75c7a16d3f2a233cbc07fb81a
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-26 15:53:54 -0300

    Ajuste para não enviar mensagens no modo dev

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -1,13 +1,17 @@
 const axios = require("axios");
 
 module.exports = async (chat_id, text) => {
-  const body = {
-    chat_id,
-    text,
-  };
+  if (!process.env.NODE_ENV) {
+    const body = {
+      chat_id,
+      text,
+    };
 
-  return await axios.post(
-    `${process.env.TELEGRAM_API}${process.env.BOT_TOKEN}/sendMessage`,
-    body
-  );
+    return await axios.post(
+      `${process.env.TELEGRAM_API}${process.env.BOT_TOKEN}/sendMessage`,
+      body
+    );
+  } else {
+    return console.log(text);
+  }
 };
