5226908ef404efb3c2698bf9fdc6d2b88716c6dd
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-26 12:13:46 -0300

    Consutas no mongo

---
+++ src/dao.js
@@ -0,0 +1,50 @@
+const Sorteio = require("./model");
+
+const createSorteio = async (name) => {
+  let sorteio = new Sorteio({
+    name,
+    participants: [],
+  });
+  await sorteio.save();
+};
+
+const exists = async (name) => {
+  let sorteio = await Sorteio.findOne({ name });
+  if (!!sorteio) {
+    return true;
+  }
+
+  return false;
+};
+
+const addParticipant = async (name, user) => {
+  Sorteio.updateOne({ name }, { $push: { participants: user } });
+};
+
+const checkParticipant = async (name, id) => {
+  let sorteio = await Sorteio.findOne({ name });
+  if (
+    sorteio.participants.findIndex((participant) => participant.id === id) ===
+    -1
+  ) {
+    return false;
+  }
+  return true;
+};
+
+const getParticipants = async (name) => {
+  return await Sorteio.findOne({ name }).participants;
+};
+
+const deleteSorteio = async (name) => {
+  return await Sorteio.deleteOne({ name });
+};
+
+module.exports = {
+  createSorteio,
+  exists,
+  addParticipant,
+  checkParticipant,
+  getParticipants,
+  deleteSorteio,
+};
