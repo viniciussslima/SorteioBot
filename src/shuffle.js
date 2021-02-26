82d7809907c02174cfd492da848125801d8b21b1
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-26 19:44:03 -0300

    Removendo o bug de uma pessoa não receber alguem

--- src/shuffle.js
+++ src/shuffle.js
@@ -3,18 +3,20 @@ const sendMessage = require("./sendMessage");
 module.exports = (participants) => {
   let loop = true;
   let index = 0;
+  let sorteados = 0;
   while (loop) {
     let sorteadoIndex = Math.floor(Math.random() * participants.length);
     let sorteado = participants[sorteadoIndex];
 
-    if (sorteado.id !== participants[index].id) {
+    if (sorteado.id !== participants[index].id && !sorteado.value) {
       sendMessage(participants[index].id, `Você tirou @${sorteado.username}`);
 
-      participants.splice(sorteadoIndex, 1);
+      sorteado.value = true;
 
       index += 1;
+      sorteados += 1;
 
-      if (participants.length === 0) {
+      if (participants.length === sorteados) {
         loop = false;
       }
     }
