b12189ea1df58971e10df3685c34c9d98a3ce754
viniciussslima999 <viniciussslima@ufrn.edu.br>
2021-02-26 22:41:38 -0300

    Agora a aplicação cria o webhook assim q inicia

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const axios = require("axios");
 const commands = require("./commands");
 
 const app = express();
@@ -16,6 +17,10 @@ db.on("error", console.error.bind(console, "connection error:"));
 app.use(express.json());
 app.post(`/${process.env.BOT_TOKEN}`, commands);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
   console.log("Api rodando na porta " + process.env.PORT);
+  await axios.post(
+    `${process.env.TELEGRAM_API}${process.env.BOT_TOKEN}/setWebhook`,
+    `${process.env.API_URL}`
+  );
 });
